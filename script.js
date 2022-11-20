e65374c89ffd4533947f0c0f91f78bc5218d2a5b
Dário Silva <dariosilva13222@gmail.com>
2022-11-20 18:56:57 +0000

    added new scroll-reveal methods

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ ScrollReveal().reveal('.body-section',{
     reset: true,
     duration:500
 });
+
+ScrollReveal().reveal('.tin-card',{
+    delay:626,
+    reset: true,
+    duration:700
+});
+
+ScrollReveal().reveal('.companie',{
+    delay:626,
+    reset: true,
+    duration:700,
+    interval:200
+});
+
+ScrollReveal().reveal('.item',{
+    delay:626,
+    reset: true,
+    duration:700,
+    interval:90
+});
+
+ScrollReveal().reveal('.small-card',{
+    delay:626,
+    reset: true,
+    duration:700,
+    interval:90
+});
